01a027b7b1a4b09d7a741e035c59547e2de79cd5
Lovely <ellaendozo457@gmail.com>
2019-03-15 14:12:38 +0800

    +User Manual

--- cw2go/index.php
+++ cw2go/index.php
@@ -190,13 +190,12 @@
         <div class="row">
             <div class="col-sm-6 col-md-4 footer-navigation">
                 <h3><a href="#"><img src="userasset/img/sojubtry.png" style="width: 226px;height: 87px;"></a></h3>
-               
                 <p class="company-name"
                     style="font-family: Lato, sans-serif;">Chicken Ween's 2go © 2019 </p>
             </div> <div class="col-md-4 footer-about">
                 <h4 style="font-family: Roboto, sans-serif;">About the company</h4>
                 <p style="font-family: Lato, sans-serif;"> Chicken Ween's 2go was establish last March 2017, that specialize in flavored chicken wings. </p>
-                <div class="social-links social-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/chickenweens2go/?hl=en"><i class="fa fa-instagram" ></i></a></div>
+                <div class="social-links social-icons"><a href="#" title="Facebook"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/chickenweens2go/?hl=en" title="Instagram"><i class="fa fa-instagram" ></i></a><a target="_blank" rel="noopener noreferrer" href="User%20Manual.pdf" title="User Manual"><i class="fa fa-book"></i></a></div>
             </div>
             <div class="col-sm-6 col-md-4 footer-contacts">
                 <div><span class="fa fa-map-marker footer-contacts-icon"> </span>
@@ -222,6 +221,7 @@
     <script src="userasset/js/Subscribe-window.js"></script>
     <!-- Go to www.addthis.com/dashboard to customize your tools -->
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5c63d0112981ca8c"></script>
+
 
 </body>
 
